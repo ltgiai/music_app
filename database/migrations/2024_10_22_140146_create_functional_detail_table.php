ee41552ce20ac9febeb8bbda54562099f2e86dc6
voanhtuan697 <anhtuanvo697@gmail.com>
2024-11-22 10:17:11 +0700

    phan quyen

--- database/migrations/2024_10_22_140146_create_functional_detail_table.php
+++ database/migrations/2024_10_22_140146_create_functional_detail_table.php
@@ -8,21 +8,23 @@ return new class extends Migration
 {
     public function up()
     {
-        // Schema::create('chi_tiet_phan_quyen', function (Blueprint $table) {
-            
-        //     $table->string('ma_phan_quyen', 7)->nullable(); // `ma_phan_quyen` - varchar(7), nullable
-        //     $table->string('ma_chuc_nang', 7)->nullable(); // `ma_chuc_nang` - varchar(5), nullable
-        //     $table->text('mo_ta_vai_tro')->nullable(); // `mo_ta_vai_tro` - text, nullable
-
-        //     // Foreign key constraints (nếu cần)
-        //     $table->foreign('ma_phan_quyen')->references('ma_phan_quyen')->on('phan_quyen')->onDelete('cascade');
-        //     $table->foreign('ma_chuc_nang')->references('ma_chuc_nang')->on('chuc_nang')->onDelete('cascade');
-        //     $table->timestamps(); // Tùy chọn: thêm created_at và updated_at
-        // });
         Schema::create('chi_tiet_phan_quyen', function (Blueprint $table) {
-            $table->foreignId('ma_phan_quyen')->constrained('phan_quyen')->onDelete('cascade');
-            $table->foreignId('ma_chuc_nang')->constrained('chuc_nang')->onDelete('cascade');
+            $table->string('ma_phan_quyen');
+            $table->string('ma_chuc_nang');
+            $table->foreign('ma_phan_quyen')
+                    ->references('ma_phan_quyen')
+                    ->on('phan_quyen')
+                    ->onDelete('cascade'); // Tự động xóa khi xóa phân quyền
+            $table->foreign('ma_chuc_nang')
+                    ->references('ma_chuc_nang')
+                    ->on('chuc_nang'); // Không cần xóa, chỉ liên kết
+            $table->boolean('xem')->default(false);
+            $table->boolean('them')->default(false);
+            $table->boolean('sua')->default(false);
+            $table->boolean('xoa')->default(false);
+            $table->timestamps();                    
         });
+        
     }
 
     public function down()
