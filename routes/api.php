28334be5f8a0e64c979eb33b7a202f8995793239
ltgiai <tuangiai52@gmail.com>
2024-11-26 21:28:48 +0700

    fixed phieuruttien ltgiai

--- routes/api.php
+++ routes/api.php
@@ -175,9 +175,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Route ArtistWithdrawalSlip
-Route::get('/artist-slip', [ArtistWithdrawalSlipController::class, 'index']);
-
-
+Route::get('/artist-slip', [ArtistWithdrawalSlipController::class, 'index']); // Liệt kê danh sách rút tiền của nghệ sĩ
+Route::get('/artist-slip/{ma_phieu}', [ArtistWithdrawalSlipController::class, 'show']); // Liệt kê danh sách rút tiền của nghệ sĩ
+Route::post('/artist-slip', [ArtistWithdrawalSlipController::class, 'store']); // Liệt kê danh sách rút tiền của nghệ sĩ
+Route::delete('/artist-slip', [ArtistWithdrawalSlipController::class, 'destroy']); // Liệt kê danh sách rút tiền của nghệ sĩ
 
 /*
 |--------------------------------------------------------------------------
